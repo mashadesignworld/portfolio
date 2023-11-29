866224b3a5b9ab65d4d5163a1b4e2f75a59beae4
mashadesignworld <njugunak.kevin@gmail.com>
2023-11-29 20:06:58 +0300

    third

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -24,7 +24,7 @@ export default function App({ Component, pageProps }) {
       </Head>
       <main className={`${montserrat.variable} font-mont bg-light dark:bg-dark w-full min-h-screen`}>
         <NavBar />
-        <AnimatePresence>
+        <AnimatePresence mode="wait">
           <Component key={router.asPath} {...pageProps} />
         </AnimatePresence>
         <Footer />
