1495557721446f325ca70aa4ae3fa0eb9d90b9b7
mashadesignworld <njugunak.kevin@gmail.com>
2024-11-03 23:29:39 +0300

    Update HireMe.js

--- src/components/HireMe.js
+++ src/components/HireMe.js
@@ -1,34 +1,29 @@
 import React from 'react';
-import { CircularText } from './icons';
 
 const HireMe = () => {
     return (
-        <div className='fixed left-4 bottom-4 flex items-center justify-center
-        overflow-hidden md:right-8
-        md:left-auto md:top-0 md:bottom-auto md:absolute'>
-            <div className='w-48 h-auto flex items-center justify-center
-            relative md:w-24'>
-                <CircularText className={"fill-dark animate-spin-slow dark:fill-light"} />
-                <a href="mailto:njugunak.kevin@gmail.com" className='flex items-center justify-center
-                absolute left-1/2 top-1/2  -translate-x-1/2 -translate-y-1/2 bg-black
-                text-white shadow-md border-solid border-dark w-20 h-20 rounded-full font-semibold
-                hover:bg-white hover:text-dark
-                                            
-                
-                dark:bg-light dark:text-dark
-                hover:dark:bg-dark hover:dark:text-light
-                hover:dark:border-light
-
-                md:w-12 md:h-12 md:text-[10px]
-                
-                
-                '>
+        <div className="fixed left-4 bottom-4 flex items-center justify-center">
+            <div className="relative w-48 h-48 flex items-center justify-center">
+                <a
+                    href="mailto:njugunak.kevin@gmail.com"
+                    className="flex items-center justify-center
+                               bg-black text-white shadow-md w-20 h-20 rounded-full font-semibold
+                               hover:bg-white hover:text-dark"
+                    style={{
+                        position: 'absolute',
+                        left: '50%',
+                        top: '50%',
+                        transform: 'translate(-50%, -50%)',
+                        zIndex: 10,
+                    }}
+                >
                     Hire Me
                 </a>
             </div>
         </div>
-    )
-}
+    );
+};
 
 export default HireMe;
+
 
